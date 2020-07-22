d90b1db9ff9d8342fa9d98742a7aab686c46eed5
JohnNils Olson <johnnils@gmail.com>
2020-07-22 10:26:21 -0700

    add instances list to place class
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- PlacesYouveBeen/Models/Place.cs
+++ PlacesYouveBeen/Models/Place.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
+
 namespace PlacesYouveBeen.Models
 {
   public class Place
   {
     public string CityName { get; set; }
+    private static List<Place> _instances = new List<Place> {};
     public Place(string cityName)
     {
       CityName = cityName;
+      _instances.Add(this);
     }
   }
 }
