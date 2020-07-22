806f008b684109644f753d100d8ff8f9f8bc73cc
JohnNils Olson <johnnils@gmail.com>
2020-07-22 10:45:18 -0700

    add routes for new place and place index
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- PlacesYouveBeen/Controllers/PlaceController.cs
+++ PlacesYouveBeen/Controllers/PlaceController.cs
@@ -6,10 +6,22 @@ namespace PlacesYouveBeen.Controllers
 {
   public class PlacesController : Controller
   {
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
     [HttpGet("/places/new")]
     public ActionResult New()
     {
       return View();
+    }
+    [HttpPost("/places")]
+    public ActionResult New(string cityName)
+    {
+      Place newPlace = new Place(cityName);
+      return RedirectToAction("Index");
     }
   }
 }
